1926229f5ccaaec87ea19ffd533a6ae7cfea2f42
trungnq1 <trungnq1@smartosc.com>
2016-01-06 17:47:56 +0700

    commit first

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/linhkien4tech/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/linhkien4tech/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Xampp\htdocs\linhkien4tech/catalog/');
+define('DIR_SYSTEM', 'D:\Xampp\htdocs\linhkien4tech/system/');
+define('DIR_DATABASE', 'D:\Xampp\htdocs\linhkien4tech/system/database/');
+define('DIR_LANGUAGE', 'D:\Xampp\htdocs\linhkien4tech/catalog/language/');
+define('DIR_TEMPLATE', 'D:\Xampp\htdocs\linhkien4tech/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\Xampp\htdocs\linhkien4tech/system/config/');
+define('DIR_IMAGE', 'D:\Xampp\htdocs\linhkien4tech/image/');
+define('DIR_CACHE', 'D:\Xampp\htdocs\linhkien4tech/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Xampp\htdocs\linhkien4tech/download/');
+define('DIR_LOGS', 'D:\Xampp\htdocs\linhkien4tech/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'linhkien4tech');
+define('DB_PREFIX', 'oc_');
+?>
